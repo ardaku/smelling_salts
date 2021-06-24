0c17977683d1221097c66ed6c547cf94ee3c5a9d
Jeron Aldaron Lau <jeronlau@plopgrizzly.com>
2021-06-23 22:52:09 -0500

    Simplify Implementation, improve method names in API.

---
+++ examples/sleep.rs
@@ -0,0 +1,103 @@
+//! This example is public domain.
+
+use smelling_salts::{Device, Watcher};
+use std::future::Future;
+use std::mem::MaybeUninit;
+use std::os::raw;
+use std::pin::Pin;
+use std::task::{Context, Poll};
+use std::time::Duration;
+
+#[repr(C)]
+struct TimeSpec {
+    sec: isize,
+    nsec: raw::c_long,
+}
+
+#[repr(C)]
+struct ITimerSpec {
+    interval: TimeSpec,
+    value: TimeSpec,
+}
+
+extern "C" {
+    fn timerfd_create(clockid: raw::c_int, flags: raw::c_int) -> raw::c_int;
+    fn timerfd_settime(
+        fd: raw::c_int,
+        flags: raw::c_int,
+        new_value: *const ITimerSpec,
+        old_value: *mut ITimerSpec,
+    ) -> raw::c_int;
+    fn read(fd: raw::c_int, buf: *mut u64, count: usize) -> isize;
+    fn close(fd: raw::c_int) -> raw::c_int;
+    fn __errno_location() -> *mut raw::c_int;
+}
+
+struct Sleep(Device, u64);
+
+impl Sleep {
+    fn new(dur: Duration) -> Self {
+        let sec = dur.as_secs() as _;
+        let nsec = dur.subsec_nanos() as _;
+
+        let timerfd = unsafe {
+            timerfd_create(1 /*Monotonic*/, 2048 /*Nonblock*/)
+        };
+        let x = unsafe {
+            timerfd_settime(
+                timerfd,
+                0,
+                &ITimerSpec {
+                    interval: TimeSpec { sec, nsec },
+                    value: TimeSpec { sec, nsec },
+                },
+                std::ptr::null_mut(),
+            )
+        };
+        assert_eq!(0, x);
+
+        Sleep(Device::new(timerfd, Watcher::new().input()), 0)
+    }
+}
+
+impl Future for Sleep {
+    type Output = ();
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<()> {
+        // Queue
+        if self.1 != 0 {
+            self.1 -= 1;
+            return Poll::Ready(());
+        }
+        // Early return if a different device woke the executor.
+        if self.0.pending() {
+            return self.0.sleep(cx);
+        }
+        // 
+        let mut x = MaybeUninit::<u64>::uninit();
+        let v = unsafe {
+            read(self.0.raw(), x.as_mut_ptr(), std::mem::size_of::<u64>())
+        };
+        if v > 0 {
+            self.1 += unsafe { x.assume_init() };
+            self.poll(cx)
+        } else {
+            self.0.sleep(cx)
+        }
+    }
+}
+
+impl Drop for Sleep {
+    fn drop(&mut self) {
+        assert_eq!(0, unsafe { close(self.0.stop()) });
+    }
+}
+
+fn main() {
+    pasts::block_on(async {
+        for _ in 0..5 {
+            println!("Sleeping for 1 second…");
+            Sleep::new(Duration::new(1, 0)).await;
+        }
+    });
+}
--- src/lib.rs
+++ src/lib.rs
@@ -21,139 +21,107 @@
 //! ### Example
 //! ```rust,no_run
 //! use smelling_salts::{Device, Watcher};
-//!
-//! use std::convert::TryInto;
 //! use std::future::Future;
-//! use std::mem;
+//! use std::mem::MaybeUninit;
 //! use std::os::raw;
 //! use std::pin::Pin;
 //! use std::task::{Context, Poll};
-//!
-//! #[allow(non_camel_case_types)]
-//! type c_ssize = isize; // True for most unix
-//! #[allow(non_camel_case_types)]
-//! type c_size = usize; // True for most unix
-//!
-//! const MAGIC_NUMBER: u32 = 0xDEAD_BEEF;
-//!
-//! // From fcntl.h
-//! const O_CLOEXEC: raw::c_int = 0o2000000;
-//! const O_NONBLOCK: raw::c_int = 0o0004000;
-//! const O_DIRECT: raw::c_int = 0o0040000;
-//!
+//! use std::time::Duration;
+//! 
+//! #[repr(C)]
+//! struct TimeSpec {
+//!     sec: isize,
+//!     nsec: raw::c_long,
+//! }
+//! 
+//! #[repr(C)]
+//! struct ITimerSpec {
+//!     interval: TimeSpec,
+//!     value: TimeSpec,
+//! }
+//! 
 //! extern "C" {
-//!     fn pipe2(pipefd: *mut [raw::c_int; 2], flags: raw::c_int) -> raw::c_int;
-//!     fn write(fd: raw::c_int, buf: *const raw::c_void, count: c_size) -> c_ssize;
-//!     fn read(fd: raw::c_int, buf: *mut raw::c_void, count: c_size) -> c_ssize;
+//!     fn timerfd_create(clockid: raw::c_int, flags: raw::c_int) -> raw::c_int;
+//!     fn timerfd_settime(
+//!         fd: raw::c_int,
+//!         flags: raw::c_int,
+//!         new_value: *const ITimerSpec,
+//!         old_value: *mut ITimerSpec,
+//!     ) -> raw::c_int;
+//!     fn read(fd: raw::c_int, buf: *mut u64, count: usize) -> isize;
 //!     fn close(fd: raw::c_int) -> raw::c_int;
+//!     fn __errno_location() -> *mut raw::c_int;
 //! }
-//!
-//! // Convert a C error (negative on error) into a result.
-//! fn error(err: raw::c_int) -> Result<(), raw::c_int> {
-//!     if err < 0 {
-//!         Err(err)
-//!     } else {
-//!         Ok(())
+//! 
+//! struct Sleep(Device, u64);
+//! 
+//! impl Sleep {
+//!     fn new(dur: Duration) -> Self {
+//!         let sec = dur.as_secs() as _;
+//!         let nsec = dur.subsec_nanos() as _;
+//! 
+//!         let timerfd = unsafe {
+//!             timerfd_create(1 /*Monotonic*/, 2048 /*Nonblock*/)
+//!         };
+//!         let x = unsafe {
+//!             timerfd_settime(
+//!                 timerfd,
+//!                 0,
+//!                 &ITimerSpec {
+//!                     interval: TimeSpec { sec, nsec },
+//!                     value: TimeSpec { sec, nsec },
+//!                 },
+//!                 std::ptr::null_mut(),
+//!             )
+//!         };
+//!         assert_eq!(0, x);
+//! 
+//!         Sleep(Device::new(timerfd, Watcher::new().input()), 0)
 //!     }
 //! }
-//!
-//! fn fd_close(fd: raw::c_int) {
-//!     // close() should never fail.
-//!     let ret = unsafe { close(fd) };
-//!     error(ret).unwrap();
-//! }
-//!
-//! // Create the sender and receiver for a pipe.
-//! fn new_pipe() -> (raw::c_int, raw::c_int) {
-//!     let [recver, sender] = unsafe {
-//!         // Create pipe for communication
-//!         let mut pipe = mem::MaybeUninit::<[raw::c_int; 2]>::uninit();
-//!         error(pipe2(pipe.as_mut_ptr(), O_CLOEXEC | O_NONBLOCK | O_DIRECT)).unwrap();
-//!         pipe.assume_init()
-//!     };
-//!
-//!     (sender, recver)
-//! }
-//!
-//! fn write_u32(fd: raw::c_int, data: u32) {
-//!     let data = [data];
-//!     let len: usize = unsafe {
-//!         write(fd, data.as_ptr().cast(), mem::size_of::<u32>())
-//!             .try_into()
-//!             .unwrap()
-//!     };
-//!     assert_eq!(len, mem::size_of::<u32>());
-//! }
-//!
-//! fn read_u32(fd: raw::c_int) -> Option<u32> {
-//!     let ret = unsafe {
-//!         let mut buffer = mem::MaybeUninit::<u32>::uninit();
-//!         let len: usize = read(fd, buffer.as_mut_ptr().cast(), mem::size_of::<u32>())
-//!             .try_into()
-//!             .unwrap_or(0);
-//!         if len == 0 {
-//!             return None;
+//! 
+//! impl Future for Sleep {
+//!     type Output = ();
+//! 
+//!     fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<()> {
+//!         // Queue
+//!         if self.1 != 0 {
+//!             self.1 -= 1;
+//!             return Poll::Ready(());
 //!         }
-//!         assert_eq!(len, mem::size_of::<u32>());
-//!         buffer.assume_init()
-//!     };
-//!     Some(ret)
-//! }
-//!
-//! pub struct PipeReceiver(Device);
-//!
-//! impl PipeReceiver {
-//!     pub fn new(fd: raw::c_int) -> Self {
-//!         PipeReceiver(Device::new(fd, Watcher::new().input()))
-//!     }
-//! }
-//!
-//! impl Drop for PipeReceiver {
-//!     fn drop(&mut self) {
-//!         // Deregister FD, then delete (must be in this order).
-//!         self.0.old();
-//!         fd_close(self.0.raw());
-//!     }
-//! }
-//!
-//! pub struct PipeFuture<'a>(&'a PipeReceiver);
-//!
-//! impl<'a> PipeFuture<'a> {
-//!     pub fn new(recver: &'a PipeReceiver) -> Self {
-//!         PipeFuture(recver)
-//!     }
-//! }
-//!
-//! impl<'a> Future for PipeFuture<'a> {
-//!     type Output = u32;
-//!
-//!     fn poll(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
-//!         if let Some(output) = read_u32((self.0).0.raw()) {
-//!             Poll::Ready(output)
+//!         // Early return if a different device woke the executor.
+//!         if self.0.pending() {
+//!             return self.0.sleep(cx);
+//!         }
+//!         // 
+//!         let mut x = MaybeUninit::<u64>::uninit();
+//!         let v = unsafe {
+//!             read(self.0.raw(), x.as_mut_ptr(), std::mem::size_of::<u64>())
+//!         };
+//!         if v > 0 {
+//!             self.1 += unsafe { x.assume_init() };
+//!             self.poll(cx)
 //!         } else {
-//!             (self.0).0.register_waker(cx.waker());
-//!             Poll::Pending
+//!             self.0.sleep(cx)
 //!         }
 //!     }
 //! }
-//!
-//! async fn async_main() {
-//!     let (sender, recver) = new_pipe();
-//!     let device = PipeReceiver::new(recver);
-//!
-//!     std::thread::spawn(move || {
-//!         std::thread::sleep(std::time::Duration::from_millis(1000));
-//!         write_u32(sender, MAGIC_NUMBER);
-//!         fd_close(sender);
-//!     });
-//!
-//!     let output = PipeFuture::new(&device).await;
-//!     assert_eq!(output, MAGIC_NUMBER);
+//! 
+//! impl Drop for Sleep {
+//!     fn drop(&mut self) {
+//!         assert_eq!(0, unsafe { close(self.0.stop()) });
+//!     }
 //! }
-//!
+//! 
 //! fn main() {
-//!     pasts::block_on(async_main());
+//!     pasts::block_on(async {
+//!         for _ in 0..5 {
+//!             println!("Sleeping for 1 second…");
+//!             Sleep::new(Duration::new(1, 0)).await;
+//!         }
+//!     });
 //! }
+//! ```
 
 #![cfg_attr(feature = "docs-rs", feature(external_doc))]
 #![cfg_attr(feature = "docs-rs", doc(include = "../README.md"))]
@@ -181,34 +149,9 @@
 )]
 
 mod device;
+mod raw;
 mod watcher;
 
-#[cfg_attr(target_arch = "wasm32", path = "ffi/web.rs")]
-#[cfg_attr(
-    not(target_arch = "wasm32"),
-    cfg_attr(target_os = "linux", path = "ffi/linux.rs"),
-    cfg_attr(target_os = "android", path = "ffi/android.rs"),
-    cfg_attr(target_os = "macos", path = "ffi/macos.rs"),
-    cfg_attr(target_os = "ios", path = "ffi/ios.rs"),
-    cfg_attr(target_os = "windows", path = "ffi/windows.rs"),
-    cfg_attr(
-        any(
-            target_os = "freebsd",
-            target_os = "dragonfly",
-            target_os = "bitrig",
-            target_os = "openbsd",
-            target_os = "netbsd"
-        ),
-        path = "ffi/bsd.rs"
-    ),
-    cfg_attr(target_os = "fuchsia", path = "ffi/fuchsia.rs"),
-    cfg_attr(target_os = "redox", path = "ffi/redox.rs"),
-    cfg_attr(target_os = "none", path = "ffi/none.rs"),
-    cfg_attr(target_os = "dummy", path = "ffi/dummy.rs")
-)]
-#[allow(unsafe_code)]
-mod ffi;
-
 pub use device::Device;
-pub use ffi::RawDevice;
+pub use raw::RawDevice;
 pub use watcher::Watcher;
